f43faafbdf6f77d2a80ac74a9bfaa955c5afd9fb
chathuranga-chandrarathna <chathuranga.chandrarathna@kogntiv.com>
2023-03-18 00:25:43 +0530

    Using beforEach setup method to structure the Angular Test cases for avoiding repetitive code

--- my-app/src/app/services/Calculator/calculator.service.spec.ts
+++ my-app/src/app/services/Calculator/calculator.service.spec.ts
@@ -2,8 +2,16 @@ import { TestBed } from '@angular/core/testing';
 import {CalculatorService} from "./calculator.service";
 import {LoggerService} from "../Logger/logger.service";
 describe('CalculatorService',() => {
+  let mockLoggerService: any;
+  let calculator: CalculatorService;
+  beforeEach(()=> {
+     console.log('calling before each');
+     mockLoggerService = jasmine.createSpyObj('loggerService',['log']);
+     calculator = new CalculatorService(mockLoggerService);
+  });
   it('should add two number', () => {
     // @ts-ignore
+    console.log('calling add');
     let mockLoggerService = jasmine.createSpyObj('loggerService',['log']);
     const calculator = new CalculatorService(mockLoggerService);
     let result = calculator.add(2,2);
@@ -12,6 +20,7 @@ describe('CalculatorService',() => {
   });
   it('should subtract two number', () => {
     // @ts-ignore
+    console.log('calling subtract');
     let mockLoggerService = jasmine.createSpyObj('loggerService',['log']);
     const calculator = new CalculatorService(mockLoggerService);
     let result = calculator.substarct(2,2);
