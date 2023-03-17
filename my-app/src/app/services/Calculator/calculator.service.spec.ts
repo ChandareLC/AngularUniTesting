2ee16448e0f90abf813e40fd3df93bc2a8b1a479
chathuranga-chandrarathna <chathuranga.chandrarathna@kogntiv.com>
2023-03-18 00:15:14 +0530

    Mocking the injected service using createSpyObj method and spy on the method

--- my-app/src/app/services/Calculator/calculator.service.spec.ts
+++ my-app/src/app/services/Calculator/calculator.service.spec.ts
@@ -4,17 +4,18 @@ import {LoggerService} from "../Logger/logger.service";
 describe('CalculatorService',() => {
   it('should add two number', () => {
     // @ts-ignore
-    let loggerService = new LoggerService();
-    const calculator = new CalculatorService(loggerService);
+    let mockLoggerService = jasmine.createSpyObj('loggerService',['log']);
+    const calculator = new CalculatorService(mockLoggerService);
     let result = calculator.add(2,2);
     expect(result).toBe(4);
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1)
   });
   it('should subtract two number', () => {
     // @ts-ignore
-    let loggerService = new LoggerService();
-    const calculator = new CalculatorService(loggerService);
+    let mockLoggerService = jasmine.createSpyObj('loggerService',['log']);
+    const calculator = new CalculatorService(mockLoggerService);
     let result = calculator.substarct(2,2);
     expect(result).toBe(0);
-
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1)
   });
 })
