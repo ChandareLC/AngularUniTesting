3510b1c47a1ebc9e2b0d6413509cf16d1a564781
chathuranga-chandrarathna <chathuranga.chandrarathna@kogntiv.com>
2023-03-18 10:14:51 +0530

    Write isolated Unit Tests cases for Angular service

--- my-app/src/app/services/Logger/logger.service.spec.ts
+++ my-app/src/app/services/Logger/logger.service.spec.ts
@@ -1,16 +1,42 @@
-import { TestBed } from '@angular/core/testing';
+import {LoggerService} from "./logger.service";
 
-import { LoggerService } from './logger.service';
-
-describe('LoggerService', () => {
+describe('LoggerService',()=>{
   let service: LoggerService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LoggerService);
-  });
+    service = new LoggerService();
+  })
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+  it('should not have any message at starting', ()=> {
+    //arrange
+   // const service = new LoggerService();
+
+    //act
+    let count = service.message.length;
+
+    //assert
+    expect(count).toBe(0);
+  })
+
+  it('should add the message when log is called', ()=> {
+    //arrange
+   // const service = new LoggerService();
+
+    //act
+    service.log('message');
+
+    //assert
+    expect(service.message.length).toBe(1);
+  })
+
+  it('should clear all the message when clear is called', ()=> {
+    //arrange
+   // const service = new LoggerService();
+    service.log('message');
+
+    //act
+    service.clear();
+
+    //assert
+    expect(service.message.length).toBe(0);
+  })
+})
