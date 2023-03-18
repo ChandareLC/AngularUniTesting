6dcc72c015262411cb3877c2367f6690a625be3d
chathuranga-chandrarathna <chathuranga.chandrarathna@kogntiv.com>
2023-03-18 18:27:45 +0530

    Create Single Post Details Component Using Input and Output property

---
+++ my-app/src/app/component/post/post.component.ts
@@ -0,0 +1,19 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Post} from "../../model/post";
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.sass']
+})
+export class PostComponent {
+  @Input() post: Post | null = null;
+  @Output() delete = new EventEmitter<void>();
+
+  constructor() { }
+  onDeletePost(event:Event){
+    event.stopPropagation();
+    this.delete.emit();
+}
+
+}
