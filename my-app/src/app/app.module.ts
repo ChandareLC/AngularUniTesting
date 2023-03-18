aa998c60bb31e79abb6e4dd0ae318bd7446a839f
chathuranga-chandrarathna <chathuranga.chandrarathna@kogntiv.com>
2023-03-18 18:27:45 +0530

    Create Single Post Details Component Using Input and Output property

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { StrenghPipe } from './pipes/Strength/strengh.pipe';
 import { PostsComponent } from './component/posts/posts.component';
 import {HttpClientModule} from "@angular/common/http";
+import { PostComponent } from './component/post/post.component';
+import {RouterLinkWithHref} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
     StrenghPipe,
-    PostsComponent
+    PostsComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterLinkWithHref
   ],
   providers: [],
   bootstrap: [AppComponent]
