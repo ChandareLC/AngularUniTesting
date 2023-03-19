8a2848a17cfa5171d9fe822bee252d66899042bc
chathuranga-chandrarathna <chathuranga.chandrarathna@kogntiv.com>
2023-03-19 23:41:55 +0530

    Difference between the DebugElement and nativeElement in ATB

--- my-app/src/app/component/post/post.component.spec.ts
+++ my-app/src/app/component/post/post.component.spec.ts
@@ -3,6 +3,7 @@ import {Post} from "../../model/post";
 import {first} from "rxjs";
 import {ComponentFixture, TestBed} from "@angular/core/testing";
 import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
 describe('Post Component', () => {
   let fixture:ComponentFixture<PostComponent>;
@@ -28,6 +29,15 @@ describe('Post Component', () => {
     const postElement: HTMLElement = fixture.nativeElement;
     const a = postElement.querySelector('a');
     expect(a?.textContent).toContain(post.title);
+  })
+
+  it('should render the post title in the anchor element using debug element', ()=>{
+    const post: Post ={ id: 1, body:'body 1',title:'title 1'};
+    comp.post=post;
+    fixture.detectChanges();
+    const postDebugElement = fixture.debugElement;
+    const aElement:HTMLElement = postDebugElement.query(By.css('a')).nativeElement;
+    expect(aElement.textContent).toContain(post.title);
   })
 
 
