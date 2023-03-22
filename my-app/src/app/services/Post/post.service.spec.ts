05ac04c57aa463961e57a419b4e68297b97e15f6
chathuranga-chandrarathna <chathuranga.chandrarathna@kogntiv.com>
2023-03-22 12:40:44 +0530

    Teting Services has HttpClient as dependency by using jasmin spy

---
+++ my-app/src/app/services/Post/post.service.spec.ts
@@ -0,0 +1,42 @@
+import {HttpClient} from "@angular/common/http";
+import {PostService} from "./post.service";
+import {of} from "rxjs";
+
+describe('Post Service',()=>{
+  let httpClientSpy : jasmine.SpyObj<HttpClient>;
+  let postService : PostService;
+  let POSTS = [
+    {
+      id: 1,
+      body: 'body 1',
+      title: 'title 1',
+    },
+    {
+      id: 2,
+      body: 'body 2',
+      title: 'title 2',
+    },
+    {
+      id: 3,
+      body: 'body 3',
+      title: 'title 3',
+    },
+  ];
+
+  beforeEach(()=>{
+    httpClientSpy= jasmine.createSpyObj('HttpClient', ['get']);
+    postService = new PostService(httpClientSpy);
+  });
+  describe('getPost',()=>{
+    it('should return expected posts when getposts is called',()=>{
+      httpClientSpy.get.and.returnValue(of(POSTS));
+      postService.getPosts().subscribe({
+        next: (posts) =>{
+          expect(posts).toEqual(POSTS);
+        },
+        error: () => {}
+      });
+      expect(httpClientSpy.get).toHaveBeenCalledTimes(1)
+    })
+  })
+})
